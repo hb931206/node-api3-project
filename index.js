e4cd113c1c1fa01bd5bd90877bca9af29a0d717f
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-06-09 19:59:35 -0400

    Installed my logger and first get function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code away!
+const express = require("express");
+const login = require("./middleware/logger");
+const userRouter = require("./users/userRouter");
+
+const server = express();
+
+server.use(login());
+server.use(userRouter);
+
+const port = 3500;
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
