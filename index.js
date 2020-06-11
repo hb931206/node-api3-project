29af9a76751690e59ce23dd31b1a69dde8d92278
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-06-10 22:30:14 -0400

    Completed the Middleware functions.

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const userRouter = require("./users/userRouter");
 
 const server = express();
 
+server.use(express.json());
 server.use(login());
 server.use(userRouter);
 
 const port = 3500;
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Whoops Something went wrong. Pleas try again later PT2	",
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -3,37 +3,57 @@ const db = require("./userDb");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  // do your magic!
+router.post("/", validateUser(), (req, res, next) => {
+  console.log(req.body);
+  db.insert(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "There was an error adding the user to the database.",
+      });
+    });
 });
 
 router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
   // do your magic!
   db.get(req.body)
     .then((user) => {
       res.status(200).json(user);
     })
     .catch((error) => {
-      res.status(500).json({
-        message: "Bad Request",
-      });
+      next(error);
     });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", validateUserId(), (req, res) => {
   // do your magic!
+  res.status(200).json(req.user);
 });
 
-router.get("/:id/posts", (req, res) => {
+router.get("/:id/posts", validatePost(), (req, res) => {
   // do your magic!
+  res.status(200).json(req.user);
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", validateUserId(), (req, res, next) => {
   // do your magic!
+  db.remove(req.params.id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({
+          message: "Bye User",
+        });
+      } else {
+        res.status(404).json({ message: "User couldn't be found" });
+      }
+    })
+    .catch(next);
 });
 
 router.put("/:id", (req, res) => {
@@ -42,15 +62,46 @@ router.put("/:id", (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId() {
   // do your magic!
+  return (req, res, next) => {
+    db.getById(req.params.id)
+      .then((user) => {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res.status(404).json({
+            message: "User not found",
+          });
+        }
+      })
+      .catch(next);
+  };
 }
 
-function validateUser(req, res, next) {
-  // do your magic!
+function validateUser() {
+  return (req, res, next) => {
+    if (!req.body) {
+      res.status(400).json({ message: "Missing user data" });
+    } else if (!req.body.name) {
+      res.status(400).json({ message: "Missing required name field" });
+    } else {
+      next();
+    }
+  };
 }
 
-function validatePost(req, res, next) {
+function validatePost() {
+  return (req, res, next) => {
+    if (!req.body) {
+      res.status(400).json({ message: "Missing Post Data" });
+    } else if (!req.text) {
+      res.status(400).json({ message: "Missing Text Field" });
+    } else {
+      next();
+    }
+  };
   // do your magic!
 }
 
